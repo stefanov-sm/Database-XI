6803f4948dcb6e9e7983241a8147217e6f59ce87
Stefan Stefanov <stefanov.sm@abv.bg>
2025-01-20 18:40:23 +0200

    Update two utility functions.sql

--- miscellaneous/two utility functions.sql
+++ miscellaneous/two utility functions.sql
@@ -4,20 +4,19 @@
 -- drop function if exists find_routine;
 create or replace function find_routine (partial_name text)
 returns table (
-	routine_schema text, 
-	routine_name text, 
+	routine_full_name text, 
 	routine_type text, 
 	return_type text, 
 	arguments text[], 
 	routine_body text
 ) as $function$
  select
-    routine_schema, routine_name,
+    format('%I.%I', routine_schema, routine_name),
     routine_type, data_type,
     (
      select array_agg(format('%s %I %s', 
               parameter_mode,
-              parameter_name,
+              coalesce(parameter_name, '$'), -- anonymous positional arguments
               data_type
             ) order by ordinal_position)
      from information_schema.parameters as p
@@ -25,19 +24,22 @@ returns table (
     ),
     routine_definition
  from information_schema.routines as r
- where routine_name ~* partial_name;
+ where routine_name ~* partial_name
+   and routine_schema not in ('information_schema', 'pg_catalog') 
+ order by routine_name, routine_schema;
 $function$ language sql;
+
+--------------------------------------------------
 
 -- drop function if exists find_table;
 create or replace function find_table (partial_name text)
 returns table (
-    table_schema text, 
-    table_name text, 
+    table_full_name text, 
     table_type text, 
     columns text[] 
 ) as $function$
  select
-    table_schema, table_name,
+    format('%I.%I', table_schema, table_name),
     table_type, 
     (
      select array_agg(concat_ws(' ',
@@ -50,5 +52,7 @@ returns table (
      where t.table_name = c.table_name 
        and t.table_schema = c.table_schema
     ) from information_schema.tables as t
- where table_name ~* partial_name;
+ where table_name ~* partial_name
+   and table_schema not in ('information_schema', 'pg_catalog')
+ order by table_name, table_schema;
 $function$ language sql;
