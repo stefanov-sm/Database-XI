2c8b7826762dd55fc817bc9f1f2b750a17c6b025
Stefan Stefanov <stefanov.sm@abv.bg>
2025-03-04 15:45:34 +0200

    Update about-ETL.sql

--- miscellaneous/About ETL/about-ETL.sql
+++ miscellaneous/About ETL/about-ETL.sql
@@ -9,12 +9,12 @@
 */
 
 -- Data extraction using COPY command
-create temporary table fl_tbl(fl text);
-copy fl_tbl from 'C:\foreign_data\fl_data.txt'
+create temporary table fw_tbl(fl text);
+copy fw_tbl from 'C:\foreign_data\fl_data.txt'
 with (format 'csv', delimiter E'\b', encoding 'Windows-1251');
 
 -- using a foreign table
-create foreign table fl_ftbl (fl text)
+create foreign table fw_ftbl (fl text)
 server file_server
 options 
 (
@@ -22,20 +22,20 @@ options
   format 'csv', delimiter E'\b', encoding 'Windows-1251'
 );
 
-select * from fl_tbl;
-select * from fl_ftbl;
+select * from fw_tbl;
+select * from fw_ftbl;
 
 /* or using INSERT
-insert into fl_tbl 
+insert into fw_tbl 
 values ('123456ABCDZ123ABCDEF1234567890AНякакъв текст А'), ('223456ABCDZ123ABCDEF1234567890AДруг текст Б'),
        ('323456ABCDZ123ABCDEF1234567890AНякакъв текст В'), ('423456ABCDZ123ABCDEF1234567890AДруг текст Г'),
        ('523456ABCDZ123ABCDEF1234567890AНякакъв текст Д');
 */
 
 
--- drop function if exists fl_regexp;
+-- drop function if exists fw_regexp;
 --------------------------------------------------------------------------------
-create or replace function fl_regexp(pos_list text)
+create or replace function fw_regexp(pos_list text)
 returns text language sql immutable as 
 $$
  select '^'
@@ -49,14 +49,14 @@ $$
 $$;
 
 -- Unit test
-select fl_regexp('1,7,12,15,11,32,21');
+select fw_regexp('1,7,12,15,11,32,21');
 
--- drop function if exists fl_array;
+-- drop function if exists fw_array;
 --------------------------------------------------------------------------------
-create or replace function fl_array(fldata text, flregexp text)
+create or replace function fw_array(fwdata text, fwregexp text)
 returns text[] language sql immutable as 
 $$
-  select regexp_match(fldata, flregexp);
+  select regexp_match(fwdata, fwregexp);
 $$;
 
 
@@ -67,17 +67,17 @@ $$;
 */
 -- data transformation in the SELECT expression list
 -- data cleansing in the WHERE clause
-create or replace temporary view fl_view as
+create or replace temporary view fw_view as
 select  arr[1]::numeric as apple, arr[2] as pear,   arr[3] as peach,
         arr[4]::integer as lemon, arr[5] as orange, arr[6] as plum,
         arr[7] as cherry 
 from
 (
- select fl_array(fl, fl_regexp('1,7,12,15,11,32,21')) as arr
- from fl_ftbl
+ select fw_array(fl, fw_regexp('1,7,12,15,11,32,21')) as arr
+ from fw_ftbl
 ) as t
 where not arr[7] ~* 'друг';
 
-select * from fl_view;
+select * from fw_view;
 
 discard all;
