f7510bb0e0faee7923baaf33c411af3db52242b0
Stefan Stefanov <stefanov.sm@abv.bg>
2025-03-11 11:32:40 +0200

    Update JSONB misc.sql

--- miscellaneous/JSONB misc.sql
+++ miscellaneous/JSONB misc.sql
@@ -4,28 +4,35 @@ returns text immutable strict language sql as
 $$
   select j #>> '{}'
 $$;
-
+-- unit test
+--------------------------------------------------------------------------------
 select ('{"key":"X100"}'::jsonb)['key'],
        jsonb_text(('{"key":"X100"}'::jsonb)['key']);
 --------------------------------------------------------------------------------
-create or replace function jsonpath_lint(jtext text, jpath text) 
-returns table (result_value text, result_type text)
-immutable strict parallel safe language plpgsql as
+
+create or replace function jsonpath_lint(jtext text, jpath text)
+returns table (result_value text, result_type text) immutable language plpgsql as
 $$
 declare
-  err_text text; err_details text; err_hint text; err_extra text;
-  LF constant text := e'\n';
+  etext text; edetails text; ehint text;
+  LF constant text := e'\n'; ERR constant text := '*** Error ***';
 begin
   return query
   with t as (select jsonb_path_query(jtext::jsonb, jpath::jsonpath) res)
     select res::text, coalesce(jsonb_typeof(res)::text, 'NULL') from t;
 exception when others then
   get stacked diagnostics
-    err_text := MESSAGE_TEXT, err_details := PG_EXCEPTION_DETAIL, 
-    err_hint := PG_EXCEPTION_HINT, err_extra := PG_EXCEPTION_CONTEXT;
-  return query select '*** Error: '||
-    err_text||LF||err_details||LF||err_hint||LF||split_part(err_extra, LF, 1), null;
+    etext := MESSAGE_TEXT, edetails := PG_EXCEPTION_DETAIL, ehint := PG_EXCEPTION_HINT;
+  return query
+  select
+    concat_ws(LF, ERR, nullif(etext, ''), nullif(edetails, ''), nullif(ehint, '')), 
+    null;
 end;
 $$;
-select * from jsonpath_lint('{"key":100}', '$.key');
+-- unit test
+--------------------------------------------------------------------------------
+select * from jsonpath_lint(
+  '{"key":[11,12,13,14,15], "value":{"current":false}}', 
+  '$.value.current'
+ );
 --------------------------------------------------------------------------------
