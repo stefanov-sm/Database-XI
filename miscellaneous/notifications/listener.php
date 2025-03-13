a19570c69fe54e5d7f831190441ead01bc46a340
Stefan Stefanov <stefanov.sm@abv.bg>
2025-03-13 21:09:47 +0200

    Update listener.php

--- miscellaneous/notifications/listener.php
+++ miscellaneous/notifications/listener.php
@@ -1,12 +1,19 @@
 <?php
-// CLI syntax: php listener.php <channel_name>
-if (PHP_SAPI == 'cli' && $argc == 2 && preg_match('/^\w+$/', $argv[1]))
-{
+if (PHP_SAPI == 'cli' && $argc == 2 && preg_match('/^\w+$/', $argv[1])) {
   $conn = new PDO('pgsql:dbname=postgres');
   $conn -> exec("LISTEN {$argv[1]}; COMMIT");
   echo "Listening channel {$argv[1]} ...".PHP_EOL;
   while(true)
-    if ($res = $conn -> pgsqlGetNotify(PDO::FETCH_ASSOC, 500))
-      echo "Notification received. Payload: {$res['payload']}".PHP_EOL;
+    if ($res = $conn -> pgsqlGetNotify(PDO::FETCH_ASSOC, 500)) {
+      $msg_data = json_decode($res['payload']);
+
+      if ($msg_data -> v > 100000) $msg_template = 'A huge one, V is %d, ID is %d';
+      else if ($msg_data -> v > 10000) $msg_template = 'A large one, V is %d, ID is %d';
+      else if ($msg_data -> v > 1000)  $msg_template = 'Big fish, V is %d, ID is %d';
+      else $msg_template = null;
+
+      if (!is_null($msg_template))
+        echo sprintf($msg_template, $msg_data -> v, $msg_data -> id).PHP_EOL;
+    }
 }
 else echo 'Error: syntax';
