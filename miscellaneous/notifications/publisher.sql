d21e292c31e2d72bbd624f433ef4358bf5340404
Stefan Stefanov <stefanov.sm@abv.bg>
2025-03-13 21:11:22 +0200

    Update publisher.sql

--- miscellaneous/notifications/publisher.sql
+++ miscellaneous/notifications/publisher.sql
@@ -1,30 +1,29 @@
-create table dropme.notifier 
+create table tests.notifier 
 (
   id integer primary key generated always as identity,
   v integer
 );
 
-create or replace function dropme.notifier_tf() returns trigger language plpgsql as
+create or replace function tests.notifier_tf() returns trigger language plpgsql as
 $$
 declare
-  msg_template text := null;
+  channel_name text;
+  message_payload text;
 begin
-  msg_template := case
-    when new.v > 100000 then 'Huge one. ID is %s, V is %s'
-    when new.v > 10000  then 'A large one. ID is %s, V is %s'
-    when new.v > 1000   then 'Big fish. ID is %s, V is %s'
-  end;
-  if msg_template is not null then
-    perform pg_notify(TG_ARGV[0], format(msg_template, new.id, new.v));
+  if new.v > 1000 then
+    CHANNEL_NAME := coalesce(TG_ARGV[0], 'general');
+    message_payload := to_json(new);
+    perform pg_notify(channel_name, message_payload);
   end if;
   return null;
 end;
+
 $$;
 
 create trigger notifier_t after insert
-on dropme.notifier for each row
-execute function dropme.notifier_tf('rita');
+on tests.notifier for each row
+execute function tests.notifier_tf('rita');
 
 -- Unit test
-insert into dropme.notifier (v) 
+insert into tests.notifier (v)
 values (10), (1001), (10001), (200000);
